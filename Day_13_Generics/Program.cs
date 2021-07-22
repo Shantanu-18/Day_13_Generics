54c32404e29ab76c99f83b5d9294fbaeaa1163f3
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-22 23:26:44 +0530

    [Shantanu] Added : Check maximum number between three floats values.

--- Day_13_Generics/Program.cs
+++ Day_13_Generics/Program.cs
@@ -9,6 +9,8 @@ namespace Day_13_Generics
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(output);
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(11.7, 26.47, 70.56);
+            Console.WriteLine(doubleoutput);
         }
     }
 }
